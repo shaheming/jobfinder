27e677dfa38cf4a9a189aef091e5bc7a5d93267e
shaheming <hemingsha@gmail.com>
2017-01-21 14:23:03 +0800

    implement table about the user's applied jobs

---
+++ app/controllers/account/jobs_controller.rb
@@ -0,0 +1,6 @@
+class Account::JobsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  	@jobs = current_user.applied_jobs
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
       resources :resumes
   	end
   end
+
+  namespace :account do
+    resources :jobs
+  end
   root "welcome#index"
 end
