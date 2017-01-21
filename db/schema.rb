fa4316bb916d3df0c2e02b7cf1aae0f9c17c882c
shaheming <hemingsha@gmail.com>
2017-01-21 11:04:59 +0800

    add detials to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120073927) do
+ActiveRecord::Schema.define(version: 20170121025323) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170120073927) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+    t.string   "location"
+    t.string   "company"
   end
 
   create_table "resumes", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,14 @@ puts "User account created."
 
 create_jobs = for i in 1..10 do 
 	Job.create!([title: "Job no.#{i}",description: "this is #{i}th public job created by seed", wage_upper_bound: rand(50..99)*100,
-		wage_lower_bound: rand(10..49)*100, is_hidden:"false"])
+		wage_lower_bound: rand(10..49)*100, is_hidden:"false",location:"Beijing",company:"Google"])
 end
 
 puts "10 Public jobs created."
 
 create_jobs = for i in 1..10 do 
 	Job.create!([title: "Job no.#{i}",description: "this is #{i}th hidden job created by seed", wage_upper_bound: rand(50..99)*100,
-		wage_lower_bound: rand(10..49)*100, is_hidden:"true"])
+		wage_lower_bound: rand(10..49)*100, is_hidden:"true",location:"Beijing",company:"Google"])
 end
 
 puts "10 Hidden jobs created."
